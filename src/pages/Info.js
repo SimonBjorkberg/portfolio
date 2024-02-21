887cf62ca137970350d365e7d3d6642a7f5e52d7
SimonBjorkberg <S.bjorkberg@gmail.com>
2024-02-21 16:41:40 +0100

    changed h1 size

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -28,7 +28,7 @@ const Info = () => {
                 <h1 className='font-extralight text-2xl mt-20 mb-4 text-neutral-400'>Experience</h1>
                 <div className='font-extralight font-mono border-b mb-5 border-neutral-700 flex justify-between h-fit'>
                     <div className='flex flex-col justify-between w-full'>
-                        <h1 className='text-4xl mb-4'>Klätterdomen - Current position</h1>
+                        <h1 className='text-2xl mb-4'>Klätterdomen - Current position</h1>
                         <p className='mb-4 w-1/2 text-neutral-400'>Working as a climbing instructor & sales rep, working with ages ranging from kids up to our most senior customers. A big part of the job is customer relations.</p>
                     </div>
                     <div className='my-auto w-44 text-right'>
@@ -37,7 +37,7 @@ const Info = () => {
                 </div>
                 <div className='font-extralight font-mono border-b mb-5 border-neutral-700 flex justify-between h-fit'>
                     <div className='flex flex-col justify-between w-full'>
-                        <h1 className='text-4xl mb-4'>Project 004 - Klätterdomen</h1>
+                        <h1 className='text-2xl mb-4'>Project 004 - Klätterdomen</h1>
                         <p className='mb-4 w-1/2 text-neutral-400'>A React web app created by me with the intention of learning TailwindCSS, SASS & playing around with layouts.</p>
                     </div>
                     <div className='my-auto w-44 text-right'>
@@ -46,7 +46,7 @@ const Info = () => {
                 </div>
                 <div className='font-extralight font-mono border-b mb-5 border-neutral-700 flex justify-between h-fit'>
                     <div className='flex flex-col justify-between w-full'>
-                        <h1 className='text-4xl mb-4'>Project 003 - E commerce</h1>
+                        <h1 className='text-2xl mb-4'>Project 003 - E commerce</h1>
                         <p className='mb-4 w-1/2 text-neutral-400'>A React web app created by me and two classmates with the intention of learning React & continuing refining backend technologies.</p>
                     </div>
                     <div className='my-auto w-44 text-right'>
@@ -55,7 +55,7 @@ const Info = () => {
                 </div>
                 <div className='font-extralight font-mono border-b mb-5 border-neutral-700 flex justify-between h-fit'>
                     <div className='flex flex-col justify-between w-full'>
-                        <h1 className='text-4xl mb-4'>Project 002 - Forum</h1>
+                        <h1 className='text-2xl mb-4'>Project 002 - Forum</h1>
                         <p className='mb-4 w-1/2 text-neutral-400'>A Handlebars web app created by me and a classmate with the intention of learning backend technologies.</p>
                     </div>
                     <div className='my-auto w-44 text-right'>
@@ -64,7 +64,7 @@ const Info = () => {
                 </div>
                 <div className='font-extralight font-mono border-b mb-5 border-neutral-700 flex justify-between h-fit'>
                     <div className='flex flex-col justify-between w-full'>
-                        <h1 className='text-4xl mb-4'>Project 001 - Dino Game</h1>
+                        <h1 className='text-2xl mb-4'>Project 001 - Dino Game</h1>
                         <p className='mb-4 w-1/2 text-neutral-400'>A game created by me and a classmate with the intention of learning JavaScript classes and it's uses.</p>
                     </div>
                     <div className='my-auto w-44 text-right'>
