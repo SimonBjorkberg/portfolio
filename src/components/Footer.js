e530c585fccefe29404f56770be644e6e0cad498
SimonBjorkberg <S.bjorkberg@gmail.com>
2024-02-28 08:50:14 +0100

    trying to satisfy lighthouse

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <div className="2xl:h-24 h-16 w-full border-t border-neutral-500">
-            <div className="w-[80%] h-full mx-auto font-sans text-sm justify-between flex text-neutral-500">
+            <div className="w-[80%] h-full mx-auto font-light text-sm justify-between flex text-neutral-300">
                 <p className="h-fit my-auto">Simon Björkberg © 2024</p>
                 <div className="flex my-auto">
                     <a className="mr-10 hover:text-white transition-all duration-500 hover:cursor-pointer" href="mailto:S.bjorkberg@gmail.com">Mail</a>
