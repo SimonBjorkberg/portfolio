244357caf42fe007830c8e236f8935a18965bf23
SimonBjorkberg <S.bjorkberg@gmail.com>
2024-03-03 21:59:36 +0100

    basic breadcrumbs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,71 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+
+    const location = useLocation();
+    const sLocation = location.pathname;
+
     return (
-        <nav className=" mx-auto flex font-mono pb-16 pl-3 pt-2 md:pt-0 md:pl-0 md:h-32 md:pb-0 md:w-[80%]">
-            <Link className="my-auto" to={"/"}>
-                <p className="text-white">Home</p>
-            </Link>
-        </nav>
+        <>
+            {
+                sLocation === "/" && <nav className="mx-auto flex font-mono pb-16 pl-3 pt-2 md:pt-0 md:pl-0 md:h-32 md:pb-0 md:w-[80%]"></nav>
+            }
+            {
+                sLocation === "/info" && <nav className="mx-auto flex font-mono pb-16 pl-3 pt-2 md:pt-0 md:pl-0 md:h-32 md:pb-0 md:w-[80%]">
+                    <div className="flex my-auto text-white">
+                        <Link to={"/"} className="text-white">
+                            Home
+                        </Link>
+                        <p className="ml-3">/</p>
+                        <p className="ml-3 text-neutral-400">Info</p>
+                    </div>
+                </nav>
+            }
+            {
+                sLocation === "/project/marketplace" && <nav className="mx-auto flex font-mono pb-16 pl-3 pt-2 md:pt-0 md:pl-0 md:h-32 md:pb-0 md:w-[80%]">
+                    <div className="flex my-auto text-white">
+                        <Link to={"/"} className="text-white">
+                            Home
+                        </Link>
+                        <p className="ml-3">/</p>
+                        <p className="ml-3 text-neutral-400">Project-003</p>
+                    </div>
+                </nav>
+            }
+            {
+                sLocation === "/project/klatterdomen" && <nav className="mx-auto flex font-mono pb-16 pl-3 pt-2 md:pt-0 md:pl-0 md:h-32 md:pb-0 md:w-[80%]">
+                    <div className="flex my-auto text-white">
+                        <Link to={"/"} className="text-white">
+                            Home
+                        </Link>
+                        <p className="ml-3">/</p>
+                        <p className="ml-3 text-neutral-400">Klatterdomen</p>
+                    </div>
+                </nav>
+            }
+            {
+                sLocation === "/project/portfolio" && <nav className="mx-auto flex font-mono pb-16 pl-3 pt-2 md:pt-0 md:pl-0 md:h-32 md:pb-0 md:w-[80%]">
+                    <div className="flex my-auto text-white">
+                        <Link to={"/"} className="text-white">
+                            Home
+                        </Link>
+                        <p className="ml-3">/</p>
+                        <p className="ml-3 text-neutral-400">Portfolio</p>
+                    </div>
+                </nav>
+            }
+            {
+                sLocation === "/project/forum" && <nav className="mx-auto flex font-mono pb-16 pl-3 pt-2 md:pt-0 md:pl-0 md:h-32 md:pb-0 md:w-[80%]">
+                    <div className="flex my-auto text-white">
+                        <Link to={"/"} className="text-white">
+                            Home
+                        </Link>
+                        <p className="ml-3">/</p>
+                        <p className="ml-3 text-neutral-400">Project-002</p>
+                    </div>
+                </nav>
+            }
+        </>
     )
 }
 
